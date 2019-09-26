79305ba891cfaeaa411208d5f28277f0707e5a2f
David <davidangulocorcuera@gmail.com>
2019-09-26 19:49:23 +0200

    working on get tag id

--- app/src/main/java/com/example/readnfctag/MainActivity.kt
+++ app/src/main/java/com/example/readnfctag/MainActivity.kt
@@ -5,11 +5,14 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.NdefMessage
+import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
-import android.nfc.Tag
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,10 +35,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+   private fun getContentFromTag(msgs: ArrayList<NdefMessage>): String {
+      val str =  msgs[0].records[0].payload
+      toReversedHex(str)
+       return toReversedHex(str)
+   }
+
+    private fun toReversedHex(bytes: ByteArray): String {
+        val stringBuilder = StringBuilder()
+        for (i in bytes.indices) {
+            if (i > 0) {
+                stringBuilder.append("")
+            }
+            val b = bytes[i].toInt() and 0xff
+            if (b < 0x10)
+                stringBuilder.append('0')
+            stringBuilder.append(Integer.toHexString(b))
+        }
+        return stringBuilder.toString()
+    }
+
+    private fun hexStringToByteArray(s: String): ByteArray {
+        val len = s.length
+        val data = ByteArray(len / 2)
+        var i = 0
+        while (i < len) {
+            data[i / 2] =
+                ((Character.digit(s[i], 16) shl 4) + Character.digit(s[i + 1], 16)).toByte()
+            i += 2
+        }
+        return data
+    }
+
+
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
-        val tag: Tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
-        tvNfcTag.text = tag.id.toString()
+        Log.v("taag",  getContentFromTag(getNdefMessagesFromIntent(intent)))
+
+
+
     }
 
     override fun onResume() {
@@ -49,4 +87,28 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         nfcAdapter.disableForegroundDispatch(this)
     }
+
+    private fun getNdefMessagesFromIntent(intent: Intent): ArrayList<NdefMessage> {
+        // Parse the intent
+        var msgs: ArrayList<NdefMessage> = ArrayList()
+        val action = intent.action
+        if (action == NfcAdapter.ACTION_TAG_DISCOVERED || action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
+            val rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            if (rawMsgs != null) {
+                for (i in rawMsgs.indices) {
+                    msgs[i] = rawMsgs[i] as NdefMessage
+                }
+
+            } else {
+                // Unknown tag type
+                val empty = byteArrayOf()
+                val record = NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty)
+                val msg = NdefMessage(arrayOf(record))
+                msgs = arrayListOf(msg)
+            }
+
+        }
+        return msgs
+    }
+
 }
